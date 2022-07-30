de53b790d37e6afc10e3ba06078f1e53a27df62a
farohan <68710283+farohan@users.noreply.github.com>
2022-07-29 17:49:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 //Welcome to app.js!
 
-/* Generating the chart */
+/* Generating the charts */
 
-const xArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-const yArray = [2, 4, 7, 3, 10, 12, 15, 14, 25, 35, 27, 32, 36, 42, 75];
+function generateStockChart() {
+    const xArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const yArray = [2, 4, 7, 3, 10, 12, 15, 14, 25, 35, 27, 32, 36, 42, 75];
 
-//Defining data
-const data = [{
-    x: xArray,
-    y: yArray,
-    mode: 'lines'
-}];
+    //Defining data
+    const data = [{
+        x: xArray,
+        y: yArray,
+        mode: 'lines'
+    }];
 
-//Defining layout
-const layout = {
-    xaxis: {range: [1, 15], title: 'Time'},
-    yaxis: {range: [0, 75], title: 'Price'},
-    title: 'Price over Time'
-};
+    //Defining layout
+    const layout = {
+        xaxis: {range: [1, 15], title: 'Time'},
+        yaxis: {range: [0, 75], title: 'Price'},
+        title: 'Price over Time'
+    };
 
-//Display new chart
-Plotly.newPlot('chart', data, layout);
+    //Display new chart
+    Plotly.newPlot('stock-chart', data, layout);
+}
 
+generateStockChart();
+
+function generateAssetsChart() {
+    const xArray = ['Equity', 'Liabilities'];
+    const yArray = [50, 50];
+
+    const layout = {title:"Assets"};
+    const data = [{labels:xArray, values:yArray, type:"pie"}];
+
+    Plotly.newPlot('assets-chart', data, layout);
+}
+
+generateAssetsChart();
 
 /* Call the Alpha Vantage API and display data */
 
@@ -38,11 +53,37 @@ function formHandler(event) {
 form.addEventListener('submit', formHandler);
 
 //Call the API
+const apiKey = 'EVD5GPKP3HYZB2FO';
+let func;
 
 function getCompanyData() {
     const inc = input.value;
-    const apiKey = 'EVD5GPKP3HYZB2FO';
-    const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${inc}&apikey=${apiKey}`;
+    func = 'OVERVIEW';
+    const url = `https://www.alphavantage.co/query?function=${func}&symbol=${inc}&apikey=${apiKey}`;
+
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    })
+}
+
+function getBalanceSheetData() {
+    const inc = input.value;
+    func = 'BALANCE_SHEET';
+    const url = `https://www.alphavantage.co/query?function=${func}&symbol=${inc}&apikey=${apiKey}`;
+
+    fetch(url).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data.quarterlyReports[0]);
+    })
+}
+
+function getStockData() {
+    const inc = input.value;
+    func = 'TIME_SERIES_MONTHLY';
+    const url = `https://www.alphavantage.co/query?function=${func}&symbol=${inc}&apikey=${apiKey}`;
 
     fetch(url).then((response) => {
         return response.json();
@@ -52,3 +93,5 @@ function getCompanyData() {
 }
 
 submitBtn.addEventListener('click', getCompanyData);
+submitBtn.addEventListener('click', getBalanceSheetData);
+submitBtn.addEventListener('click', getStockData);
